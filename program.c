3b030506f9af836810eb10549f7299432b038bf1
Hammad Majid <hm.hammadmajid@gmail.com>
2022-10-26 06:02:03 +0000

    Update program.c

--- program.c
+++ program.c
@@ -14,26 +14,18 @@ int main()
 	// remove unwanted \n from string
 	strtok(original_input, "\n");
 
-	// convert string to lowercase
-	int input_len = strlen(original_input);
-	for (int i = 0; i < input_len; i++)
-	{
-		original_input[i] = tolower(original_input[i]);
-	}
-
-	// create a copy of original string
-	char reversed_input[LEN];
-	strcpy(reversed_input, original_input);
-
 	// reverse the string
-	for (int j = input_len; j >= 0; j--)
+	int input_len = strlen(original_input);
+	char reversed_input[LEN];
+	int k = 0;
+	for (int j = input_len - 1; j >= 0; --j)
 	{
-		// TODO: reverse the string
+		reversed_input[k] = original_input[j];
+		k++;
 	}
 
-	// strcmp will return 0 if both strings are same;
-	int is_same = 0;
-	if (strcmp(original_input, reversed_input) == is_same)
+	// compare and print the strings
+	if (strcmp(original_input, reversed_input) == 0)
 		printf("Orginal: %s\nReversed: %s\nIs Palindrome\n", original_input, reversed_input);
 	else
 		printf("Orginal: %s\nReversed: %s\nIs Not Palindrome\n", original_input, reversed_input);
