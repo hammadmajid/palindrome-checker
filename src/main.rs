b9cfde96778f64d03e64cb18482c64aa6e06aeee
hammadmajid <hm.hammadmajid@gmail.com>
2022-10-28 10:16:00 +0500

    refactor read promt

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 use std::io::{stdin, stdout, Write};
 fn main() {
-    let input: String = read_from_prompt();
-    print!("Your Input: {input}");
+    #[allow(unused_mut)]
+    let mut input: String = read_from_prompt("Enter a String>".to_string());
+    print!("Your Input: {}", input);
 }
 
-fn read_from_prompt() -> String {
-    print!("Enter a string: ");
-    stdout().flush().unwrap();
-    let mut s: String = String::new();
-    stdin().read_line(&mut s).expect("Failed to read line");
-    while s.ends_with('\n') || s.ends_with('\r') {
-        s.pop();
+fn read_from_prompt(msg: String) -> String {
+    {
+        print!("{} ", msg);
+        stdout().flush().unwrap();
     }
-    return s;
+    {
+        let mut s: String = String::new();
+        stdin().read_line(&mut s).expect("Failed to read line");
+        {
+            while s.ends_with('\n') || s.ends_with('\r') {
+                s.pop();
+            }
+        }
+        return s;
+    }
 }
