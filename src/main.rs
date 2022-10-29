3d910c0738052f00e49d7e26717d5ed7ffddd73e
hammadmajid <hm.hammadmajid@gmail.com>
2022-10-29 18:02:45 +0500

    Style improve readability

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 pub mod console;
 pub mod options;
 fn main() {
-    let msg: String = "Enter a String:".to_string();
-    console::print_line(msg);
-    let mut input: String = console::read_line();
-    input = options::options(input);
-    if input == input.chars().rev().collect::<String>() {
-        print!("Is Palindrome");
-    } else {
-        print!("Not a Palindrome");
+    {
+        let msg: String = "Enter a String:".to_string();
+        console::print_line(msg);
+    }
+    {
+        let mut input: String = console::read_line();
+        input = options::options(input);
+        
+        if input == input.chars().rev().collect::<String>() {
+            print!("Is Palindrome");
+        } else {
+            print!("Not a Palindrome");
+        }
     }
 }
